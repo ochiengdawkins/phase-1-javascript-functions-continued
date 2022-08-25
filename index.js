b5c6652096d7d5650d1a40b6720543c664f000fa
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 09:38:26 +0300

    commit tree 4b4f32741ae109e8bbb4fa2d9d695206671f98a6

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity){
+    let answer= 'This Saturday, I want to ';
+    if(activity===undefined)
+        return answer+ 'roller-skate!';
+    else return answer+activity+'!';
+}
+
+const mondayWork=function(monday){
+    let answer2 = 'This Monday, I will ';
+    if(monday===undefined)
+        return answer2+'go to the office.';
+    else return answer2+monday+'.';
+}
+
+function wrapAdjective(flair='*'){
+  const innerFunction= function(adjective='special'){
+      return 'You are '+flair+adjective+flair+'!'; 
+  }
+  return innerFunction;
+}
